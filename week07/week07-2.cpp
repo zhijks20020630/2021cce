75cb98429ee47aa014ffd4d81a93c782d1dd5ad2
ZHI <91170838+zhijks20020630@users.noreply.github.com>
2021-10-27 12:03:49 +0800

    Add files via upload

---
+++ week07/week07-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n=10;
+//if(n>0)
+//{
+//  printf("n大於0\n");
+//}
+    while(n>0)
+    {
+        printf("n大於0\n");
+    }
+}
---
+++ week07/week07-4.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    for (int n=7;n>0;n--)
+        {
+            printf("n¤j©ó0\n");
+        }
+}
+
