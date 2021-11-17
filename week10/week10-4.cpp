fc3dc09779dc460d494151d7aad5bdaf7e1012a4
ZHI <91170838+zhijks20020630@users.noreply.github.com>
2021-11-17 11:52:07 +0800

    Add files via upload

---
+++ week10/week10-4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int a[10000000]={};
+
+int main()
+{
+    int ans=0;
+    for(int i=2;i<10000000;i++){
+        if(a[i]==0){
+            ans++;
+            for(int k=i+i;k<10000000;k=k+i){
+                a[k]=1;
+            }
+        }
+    }
+    printf(" Ans: %d ",ans);
+}
